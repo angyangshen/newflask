9fe49072541903c04bd0331c346e89bc6c5783cf
angyangshen <angyangshen@gmail.com>
2019-11-06 15:53:51 +0800

    css, contact and profile

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = "Sexy"
-    return render_template('index.html', name=name)
+    first_name = "Adrian"
+    signed_in = True
+    return render_template('index.html', first_name=first_name, signed_in = signed_in)
+
+@app.route('/users/<username>')
+def profile(username):
+    return render_template("profile_page.html", username=username)
 
 @app.route('/hello')
 def hello():
     return "hello again"
-if __name__ == "__main__":
-    app.run(debug=True)
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+    
+if __name__ == "__main__":
+    app.run()
